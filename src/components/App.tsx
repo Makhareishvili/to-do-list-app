dc312f3347e87efe2ef60d142a3a151557603ea5
Makhareishvili <levanimakharei7li@gmail.com>
2023-03-20 18:13:57 +0400

    first commit

---
+++ src/components/App.tsx
@@ -0,0 +1,12 @@
+import "../sass/components/App.sass";
+const App = () => {
+  return (
+    <div className="App">
+      <div className="mainContainer">
+        <p>Y</p>
+        <p>Y</p>
+      </div>
+    </div>
+  );
+};
+export default App;
